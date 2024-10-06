7a6aac38113327f8218edf85d3a2c911a53c45c7
Jonas da Silva <phoenixr41414141@gmail.com>
2024-10-06 20:08:34 +0200

    ✨ Add rustaceanvim plugin

--- dot-config/nvim/lua/plugins/init.lua
+++ dot-config/nvim/lua/plugins/init.lua
@@ -62,6 +62,12 @@ return {
     },
   },
 
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5",
+    lazy = false, -- This plugin is already lazy
+  },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
