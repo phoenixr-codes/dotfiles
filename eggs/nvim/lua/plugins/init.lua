b506d226b38d73b058bfe0ad6853bee397ec0fc1
Jonas da Silva <phoenixr41414141@gmail.com>
2025-02-27 16:30:38 +0100

    🔧 Add lorem plugin

--- eggs/nvim/lua/plugins/init.lua
+++ eggs/nvim/lua/plugins/init.lua
@@ -109,4 +109,16 @@ return {
   {
     "b0o/schemastore.nvim",
   },
+
+  {
+    "derektata/lorem.nvim",
+    config = function()
+      require("lorem").opts {
+        sentenceLength = "medium",
+        comma_chance = 0.2,
+        max_commas_per_sentence = 2,
+      }
+    end,
+    lazy = false,
+  },
 }
