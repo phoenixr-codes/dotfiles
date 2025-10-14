0bb36dca520d753671b3c70753a808689b6df5c0
Jonas da Silva <phoenixr41414141@gmail.com>
2025-10-14 20:28:43 +0200

    👽️ Migrate nvim lsp api

--- eggs/nvim/lua/configs/lspconfig.lua
+++ eggs/nvim/lua/configs/lspconfig.lua
@@ -1,14 +1,14 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = vim.lsp.config
 
 local servers = {
   "html",
   "cssls",
   "pylsp",
   "ruby_lsp",
-  "roc_ls",
+  --"roc_ls",
   "vls",
   "ccls",
   "marksman",
@@ -16,7 +16,7 @@ local servers = {
   "nim_langserver",
   "bashls",
   "tinymist",
-  "gleam",
+  --"gleam",
   "zls",
   "nushell",
   "metals",
@@ -25,41 +25,46 @@ local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig(lsp, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
+  vim.lsp.enable(lsp)
 end
 
-lspconfig.java_language_server.setup {
+lspconfig("java_language_server", {
   cmd = { "java-language-server" },
-}
+})
+vim.lsp.enable("java_language_server")
 
-lspconfig.jsonls.setup {
+lspconfig("jsonls", {
   settings = {
     json = {
       schemas = require("schemastore").json.schemas(),
       validate = { enable = true },
     },
   },
-}
+})
+vim.lsp.enable("jsonls")
 
-lspconfig.denols.setup {
+lspconfig("denols", {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
-  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-}
+  root_markers = { { "deno.json", "deno.jsonc" } },
+})
+vim.lsp.enable("denols")
 
-lspconfig.ts_ls.setup {
+lspconfig("ts_ls", {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
-  root_dir = lspconfig.util.root_pattern "package.json",
+  root_markers = { "package.json" },
   capabilities = nvlsp.capabilities,
   single_file_support = false,
-}
+})
+vim.lsp.enable("ts_ls")
 
-lspconfig.taplo.setup {
+lspconfig("taplo", {
   settings = {
     evenBetterToml = {
       schema = {
@@ -68,4 +73,5 @@ lspconfig.taplo.setup {
       },
     },
   },
-}
+})
+vim.lsp.enable("taplo")
